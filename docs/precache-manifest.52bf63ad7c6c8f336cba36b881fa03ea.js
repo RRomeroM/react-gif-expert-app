c5425e0c5cf728ffa11994702621cf5076250482
Romulo Romero <romuloromero89@gmail.com>
2020-08-11 23:53:46 -0300

    Docs agregado

---
+++ docs/precache-manifest.52bf63ad7c6c8f336cba36b881fa03ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f83f93e1096628ccb7d6d3793faf33e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02f14005db7576c0f69f",
+    "url": "/static/css/main.96752784.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f14005db7576c0f69f",
+    "url": "/static/js/main.0c907d46.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
